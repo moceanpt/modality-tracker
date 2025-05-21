b86b31042041e3f26373fae9008e6cef65ad0b08
Joshua Park <moceanpt@Joshuas-MBP.home>
2025-05-20 20:37:35 -0400

    chore(styles): add blink animation

--- modality-tracker-frontend/pages/_app.tsx
+++ modality-tracker-frontend/pages/_app.tsx
@@ -1,5 +1,7 @@
 // pages/_app.tsx
 import '@/styles/globals.css';
+import '../styles/blink.css';
+
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
