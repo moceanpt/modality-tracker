59a6fa5bfa3062d94b60385ff757a78e2cfa7b17
Joshua Park <moceanpt@Joshuas-MBP.localdomain>
2025-05-21 17:17:52 -0400

    chore: remove duplicate .js pages to stop runtime conflicts

--- modality-tracker-frontend/pages/_app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = MyApp;
-// pages/_app.tsx
-require("@/styles/globals.css");
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps}/>;
-}
