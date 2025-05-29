35c68aeff20259021ada8af2a168f41ec3aeb5e2
Joshua Park <joshuapark37@gmail.com>
2025-05-29 16:56:58 -0400

    fix: guard onViewNote callback (build error)

--- modality-tracker-frontend/components/ClientCard.tsx
+++ modality-tracker-frontend/components/ClientCard.tsx
@@ -42,7 +42,7 @@ export default function ClientCard({
     {/* 📓 icon opens note viewer */}
     {c.note && (
       <button
-        onClick={() => onViewNote?.(c.note)}
+        onClick={() => c.note && onViewNote?.(c.note)}
         title="View note"
         className="text-base leading-none focus:outline-none"
       >
